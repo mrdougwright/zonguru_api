929d68c570d2c87f3fef9d28afdfc8a9887a2eb8
Doug Wright <mrdougwright@gmail.com>
2017-03-01 08:33:14 -0800

    working aws api wrapper

---
+++ app/models/amazon_api.rb
@@ -0,0 +1,26 @@
+class AmazonApi
+  attr_reader :request
+
+  def initialize
+    @request = init_request_obj
+  end
+
+  def item_lookup(asin, *groups)
+    request.item_lookup(
+      query: {
+        'ItemId' => asin.to_s,
+        'ResponseGroups' => groups.join(',')
+      }
+    )
+  end
+
+
+  private
+
+  def init_request_obj
+    r = Vacuum.new
+    r.associate_tag = 'tag'
+    r
+  end
+
+end
